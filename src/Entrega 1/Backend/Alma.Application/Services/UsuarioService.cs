247796c8ac098bb9fb50b6ab64204f22e618f5b8
Nicolas Hayato Nitta <25027686@fecap.br>
2025-08-26 22:31:13 -0300

    Feat Terminando estrutura projeto

---
+++ src/Entrega 1/Backend/Alma.Application/Services/UsuarioService.cs
@@ -0,0 +1,6 @@
+﻿namespace Alma.API.Controller
+{
+    public class IUsuarioService
+    {
+    }
+}
---
+++ src/Entrega 1/Backend/Alma.Domain/Enum/TipoUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alma.Domain.Enum
+{
+ public enum TipoUsuario
+    {
+        Admin = 0,
+        comum = 1,
+    }
+}
